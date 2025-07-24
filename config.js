04d0116bb9c2193922379b62dca42a96a55d3e4c
Quilik8 <proyecto001a.u.t.o.r@gmail.com>
2025-07-24 00:50:09 -0500

    Configurado e integrado el cliente de Supabase en el proyecto

---
+++ config.js
@@ -0,0 +1,7 @@
+// --- CONFIGURACIÓN DE SUPABASE ---
+
+const SUPABASE_URL = "https://dyjuvsqghhjtgzbspglz.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR5anV2c3FnaGhqdGd6YnNwZ2x6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzMzQwNjksImV4cCI6MjA2ODkxMDA2OX0.FmhuMYeYf4wuJtuwz6XX_ZI3_AORepwp3_bTXRM5c2Y";
+
+// Creamos un cliente de Supabase que usaremos en todo el proyecto
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
